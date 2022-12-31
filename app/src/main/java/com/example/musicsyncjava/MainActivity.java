2665836bdd5d27c328a01705a48981b863bf30ee
WerPre <308141@o365.stud.umk.pl>
2022-12-31 01:22:46 +0100

    prosty odtwarzacz jednej piosenki (brak stop)

--- app/src/main/java/com/example/musicsyncjava/MainActivity.java
+++ app/src/main/java/com/example/musicsyncjava/MainActivity.java
@@ -9,8 +9,11 @@ The code uses NTP client to query the NTP server and captues the offset of the a
 package com.example.musicsyncjava;
 
 import android.Manifest;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,11 +21,16 @@ import androidx.core.app.ActivityCompat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
 public class MainActivity extends AppCompatActivity {
     //stores the offset value
     long offset =0L;
 
     String ntpHost = "17.253.26.253";//"time.apple.com";
+
+    boolean synced =false;
+
+    MediaPlayer mp = new MediaPlayer();
 
     private final String [] permissions = {
             Manifest.permission.INTERNET
@@ -47,9 +55,39 @@ public class MainActivity extends AppCompatActivity {
 
         //thread to calculate offset with respect to the android device monotonic time
         Thread calculate_offsets = new Thread(Timer);
+        Thread waiting = new Thread(Wait_for_sync);
+
         calculate_offsets.setPriority(10);
+        waiting.setPriority(8);
         calculate_offsets.start();
+        waiting.start();
+
+        //Wait_for_sync_manager.start();
     }//end onCreate
+
+    public void OnClick_play(View v){
+        mp.start();
+
+        final Button btn_back = findViewById(R.id.button_back);
+        final Button btn_next = findViewById(R.id.button_next);
+
+        btn_back.setEnabled(true);
+        btn_next.setEnabled(true);
+    }
+
+    public void OnClick_change_music_position(View v){
+        final Button btn_back = findViewById(R.id.button_back);
+        final Button btn_next = findViewById(R.id.button_next);
+
+        if (v.getId() == btn_back.getId())
+            mp.seekTo(mp.getCurrentPosition() - 10);
+        else if(v.getId() == btn_next.getId())
+            mp.seekTo(mp.getCurrentPosition() + 10);
+    }
+
+    void set_music(){
+        mp = MediaPlayer.create(getApplicationContext(), R.raw.zelda);
+    }
 
     long get_ntp_offset()
     {
@@ -92,7 +130,27 @@ public class MainActivity extends AppCompatActivity {
                 String dateString = formatter.format(new Date(System.currentTimeMillis() + offset));
                 final TextView textViewToChange = (TextView) findViewById(R.id.textView);
                 textViewToChange.setText(dateString);
+                synced = true;
             }
         }//end  while (run_sys_time)
     };//end Timer
+
+    Runnable Wait_for_sync = new Runnable() {
+        @Override
+        public void run() {
+            while (true) {
+                if (synced) {
+                    set_music();
+                    System.out.println("hejo");
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            final Button button = (Button) findViewById(R.id.button_play);
+                            button.setEnabled(true);
+                        }
+                    });
+                    break;
+                }
+            }    //end while true
+        }//end run
+    }; //end Wait_for_sync
 }//end MainActivity
